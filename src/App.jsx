a5b529f772191d90eb5addd4db6a38c96e9f44f9
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-04 17:22:42 -0300

    home +

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/header/header';
 import Navbar from './components/nav/nav';
+import Home from './components/home/home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="bg-circle2"></div>
       <Header />
       <Navbar />
+      <Home />
     </>
   );
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,43 @@
 import './home.css';
-import img from './props/img2.jpg';
+// import img from './props/img2.jpg';
 import {BsMouse} from 'react-icons/bs'
 
 function Home (){
-    return ()
+    return (
+        <div id='home' className="container home-container">
+            <div className="logo">
+                <div className="main-img">
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                    <span><div className="circle"></div></span>
+                </div>
+                    <img src='{img}' alt='...'/>
+                </div>
+                <a href='#footer' className="scroll-down">
+                    <hr/>
+                    <h5>scroll down</h5>
+                    <BsMouse className="scroll"/>
+                    <hr/>
+                </a>
+                <h2> <span>About Me </span> <br/>
+                <p>
+                    I am a Front-End Web Developer with a passion for creating beautiful and functional websites. I like to create websites that are user friendly and easy to use. I have a strong background in HTML, CSS, JavaScript, and React. I am also familiar with the following technologies: Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos deleniti repellat officia architecto dicta, eum voluptates voluptatibus, fuga iusto autem consectetur temporibus neque repellendus recusandae voluptatem aliquam, esse nostrum sint.
+                </p>
+                </h2>
+        
+        </div>
+    )
 }
 
 
+// const toggle = document.querySelector('.main-img');
+// toggle.addEventListener('click', () => {
+//     toggle.classList.toggle('active');
+// });
 
 export default Home;
